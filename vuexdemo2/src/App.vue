351f12d2cb58319ddf72779fe0f486db846d10c9
FelixXG <felixxg@foxmail.com>
2019-05-23 00:22:09 +0800

    vuex的应用

---
+++ vuexdemo2/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+
+    </div>
+</template>
+
+<script>
+import ProductList from "./components/product-list";
+import ShoppingCart from "./components/shoppingcart";
+
+export default {
+    name: "App",
+    components: {
+        ProductList,
+        ShoppingCart
+    },
+    computed: mapState({
+        email: state => state.userInfo.email
+    })
+};
+</script>
+
+<style>
+#app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ vuexdemo2/src/components/product-list.vue
@@ -0,0 +1,45 @@
+<template>
+    <ul>
+        <li v-for="product in products" :key="product.id">
+            {{product.title}} - {{product.price}}
+            <br>
+            <button :disabled="!product.inventory" @click="addProductToCart(product)">
+                加入购物车
+            </button>
+        </li>
+    </ul>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+
+export default {
+    data() {
+        return {};
+    },
+
+    components: {},
+
+    computed: mapState({
+        products: state => state.products.all
+    }),
+
+    // computed: {
+    //     products() {
+    //         return this.$store.state.products.all;
+    //     }
+    // },
+
+    created() {
+        this.$store.dispatch("products/getAllProducts");
+    },
+    methods: mapActions("cart", ["addProductToCart"]),
+    methods: {
+        addProductToCart(product) {
+            this.$store.dispatch("cart/addProductToCart", product);
+        }
+    }
+};
+</script>
+<style lang='less' scoped>
+</style>
