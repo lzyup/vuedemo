cbf89a1920781299a18f01ab189afe099cae97ff
FelixXG <felixxg@foxmail.com>
2019-07-31 16:58:03 +0800

    dep

--- vuebinding/public/js/observer.js
+++ vuebinding/public/js/observer.js
@@ -50,19 +50,23 @@ function observer(value, vm) {
 
 var uid = 0
 
-
+//Dep这个类是订阅者（观察者的依赖）的管理器
 function Dep() {
     this.id = uid++;
+    // subs管理订阅器即watcher
     this.subs = [];
 }
 
 //通常把订阅者自身当成引用传入发布者对象中，同时订阅者对象还需要提供一个名为诸如updata的方法，供发布者对象在合适的时候调用
 
 Dep.prototype = {
+    //这里的sub是订阅者即是watcher
     addSub: function (sub) {
         this.subs.push(sub);
     },
 
+    //检查当前Dep.target是否存在以及判断这个watcher已经被添加到相应的依赖当中，如果没有则添加订阅者（依赖），如果已经被添加了那么就不做处理
+    //这里的this是调用depend()函数的对象即是Dep()构造函数的实例对象dep
     depend: function () {
         Dep.target.addDep(this);
     },
